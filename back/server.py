2fe3a27456fc19b2459a08083d5700ae4bbbe2a1
Julia Fu <juliafu@Julias-MacBook-Pro-1160.local>
2023-05-01 11:25:57 -0400

    fix server

--- back/server.py
+++ back/server.py
@@ -3,24 +3,26 @@ from flask import Flask, request, jsonify, json
 
 # reads the json file with user data
 
-user_data = [json.loads(line) for line in open('user_data.json', 'r')]
+user_data = [json.loads(line) for line in open('user_data.json', 'r')][0]
 
 # reads the json file with post data
-post_data = [json.loads(line) for line in open('post_data.json', 'r')]
+post_data = [json.loads(line) for line in open('post_data.json', 'r')][0]
 
 app = Flask(__name__)
 
+print(user_data[0])
+
 # view function to search posts by a specific user
 @app.route('/search/<int:uid>', methods = ['GET'])
-def search_user(id):
+def search_user(uid):
     if request.method == 'GET':
-        if id<0:
+        if uid<0:
             'Invalid user id', 404
         else:
             if len(post_data) > 0:
                 searched_posts = []
                 for post in post_data:
-                    if post['posted_by'] == id:
+                    if post['posted_by'] == uid:
                         searched_posts.append(post)
                 return jsonify(searched_posts), 200
             else:
@@ -47,4 +49,4 @@ def search_posts(keyword):
         'Invalid request', 500 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
