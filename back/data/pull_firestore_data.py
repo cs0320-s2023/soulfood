ae9252a420d142e74aa41462a2a6cceef674ddc7
naicheng_he <arniehe2004@gmail.com>
2023-05-05 13:53:07 -0400

    fixed pulling from firestore

--- back/data/pull_firestore_data.py
+++ back/data/pull_firestore_data.py
@@ -3,33 +3,38 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import json
 
-cred = credentials.Certificate("data\soulfood-59503-firebase-adminsdk-erydk-5610b1b065.json")
-firebase_admin.initialize_app(cred)
+def update_firestore_data(app):
 
-db = firestore.client()
+    db = firestore.client(app)
 
-posts_ref = db.collection('posts')
-docs = posts_ref.stream()
+    posts_ref = db.collection('posts')
+    docs = posts_ref.stream()
 
-# Convert documents to a dictionary
-posts = {}
-for doc in docs:
-    posts[doc.id] = doc.to_dict()
+    # print(docs)
 
-users_ref = db.collection('profiles')
-docs = users_ref.stream()
+    # Convert documents to a dictionary
+    posts = []
+    for doc in docs:
+        posts.append(doc.to_dict())
 
-# Convert documents to a dictionary
-users = {}
-for doc in docs:
-    users[doc.id] = doc.to_dict()
+    users_ref = db.collection('profiles')
+    docs = users_ref.stream()
+    
+    # print(docs)
 
-file1 = open('data/user_data.json', 'w')
-j_user = json.dumps(users)
-file1.write(j_user)
-file1.close()
+    # Convert documents to a dictionary
+    users = []
+    for doc in docs:
+        users.append(doc.to_dict())
 
-file2 = open('data/post_data.json', 'w')
-j_post = json.dumps(posts)
-file2.write(j_post)
-file2.close()
+    file1 = open('data/user_data.json', 'w')
+    j_user = json.dumps(users[0:len(users)-1])
+    file1.write(j_user)
+    file1.close()
+
+    file2 = open('data/post_data.json', 'w')
+    j_post = json.dumps(posts[0:len(posts)-1])
+    file2.write(j_post)
+    file2.close()
+
+# update_firestore_data()
--- back/server/server.py
+++ back/server/server.py
@@ -4,22 +4,32 @@ from search_post_key import search_post_by_keyword
 from search_post_label import search_post_by_label
 from search_user import search_user
 from recommend import recommend_posts
-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+import sys
+sys.path.append("data")
+import pull_firestore_data
 
 # creates flask app
 app = Flask(__name__)
 
+cred = credentials.Certificate("data\soulfood-59503-firebase-adminsdk-erydk-5610b1b065.json")
+fireapp = firebase_admin.initialize_app(cred)
 
 # view function to search for posts by a specific user
 # endpoint: search/post
 @app.route('/search/post/<int:uid>', methods = ['GET'])
 def Search_Post_By_User(uid):
+    pull_firestore_data.update_firestore_data(fireapp)
     return search_post_by_user(uid)
 
 # view function to search for posts with certain keywords in the paragraphs or titles
 # endpoint: search/post
 @app.route('/search/post/<string:keyword>', methods = ['GET'])
 def Search_Post_By_Keyword(keyword):
+    pull_firestore_data.update_firestore_data(fireapp)
     return search_post_by_keyword(keyword)
 
 # @app.route('/search/post/<none>', methods = ['GET'])
@@ -30,21 +40,24 @@ def Search_Post_By_Keyword(keyword):
 # endpoint: search/label
 @app.route('/search/label/<string:label>', methods = ['GET'])
 def Search_Post_By_Label(label):
+    pull_firestore_data.update_firestore_data(fireapp)
     return search_post_by_label(label)
     
 #  view function to search for a particular user by user id 
 # endpoint: search/user
 @app.route('/search/user/<int:uid>', methods = ['GET'])
 def Search_User(uid):
+    pull_firestore_data.update_firestore_data(fireapp)
     return search_user(uid)
 
 # view function to recommend posts
 # endpoint: recommend
 @app.route('/recommend/<int:uid>/<int:num_recommend>', methods=['GET'])
 def Recommend_Posts(uid, num_recommend):
+    pull_firestore_data.update_firestore_data(fireapp)
     return recommend_posts(uid, num_recommend)
-            
-        
+
 
 if __name__ == '__main__':
+    pull_firestore_data.update_firestore_data(fireapp)
     app.run(debug=True)
