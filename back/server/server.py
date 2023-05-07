adf1b0a04d15eccd56f08fb43e03a965be01711c
naicheng_he <arniehe2004@gmail.com>
2023-05-06 20:10:51 -0400

    server runs

--- back/server/server.py
+++ back/server/server.py
@@ -29,7 +29,7 @@ fireapp = firebase_admin.initialize_app(cred)
 @app.route('/search/post/<int:uid>', methods = ['GET'])
 @cross_origin()
 def Search_Post_By_User(uid):
-    pull_firestore_data.update_firestore_data(fireapp)
+    # pull_firestore_data.update_firestore_data(fireapp)
     return search_post_by_user(uid)
 
 # view function to search for posts with certain keywords in the paragraphs or titles
@@ -37,7 +37,7 @@ def Search_Post_By_User(uid):
 @app.route('/search/post/<string:keyword>', methods = ['GET'])
 @cross_origin()
 def Search_Post_By_Keyword(keyword):
-    pull_firestore_data.update_firestore_data(fireapp)
+    # pull_firestore_data.update_firestore_data(fireapp)
     return search_post_by_keyword(keyword)
 
 # when no uid query is provided
@@ -51,7 +51,7 @@ def Search_Post_By_Key_Empty():
 @app.route('/search/label/<string:label>', methods = ['GET'])
 @cross_origin()
 def Search_Post_By_Label(label):
-    pull_firestore_data.update_firestore_data(fireapp)
+    # pull_firestore_data.update_firestore_data(fireapp)
     return search_post_by_label(label)
 # when label query is not given
 @app.route('/search/label/', methods = ['GET'])
@@ -64,7 +64,7 @@ def Search_Post_By_Label_Empty():
 @app.route('/search/user/<int:uid>', methods = ['GET'])
 @cross_origin()
 def Search_User(uid):
-    pull_firestore_data.update_firestore_data(fireapp)
+    # pull_firestore_data.update_firestore_data(fireapp)
     return search_user(uid)
 # when user id query is not given
 @app.route('/search/user/', methods = ['GET'])
@@ -77,7 +77,7 @@ def Search_User_Empty():
 @app.route('/recommend/<int:uid>/<int:num_recommend>', methods=['GET'])
 @cross_origin()
 def Recommend_Posts(uid, num_recommend):
-    pull_firestore_data.update_firestore_data(fireapp)
+    # pull_firestore_data.update_firestore_data(fireapp)
     return recommend_posts(uid, num_recommend)
 # when both uid and num_recommend are not given
 @app.route('/recommend/', methods = ['GET'])
@@ -94,5 +94,5 @@ def Recommend_Posts_Default(uid):
 
 
 if __name__ == '__main__':
-    pull_firestore_data.update_firestore_data(fireapp)
+    # pull_firestore_data.update_firestore_data(fireapp)
     app.run(debug=True)
